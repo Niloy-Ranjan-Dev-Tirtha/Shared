ebf8d84cd504be05fe04d5af194d202b90062af5
Niloy Ranjan Dev Tirtha <109666659+Niloy-Ranjan-Dev-Tirtha@users.noreply.github.com>
2023-01-30 20:30:46 +0600

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,21 @@
+class Circle {
+    public float area(float radious) {
+        float circleArea = (3.1416f * (radious * radious));
+        return circleArea;
+    }
+}
+
+class Cylinder extends Circle {
+    public float volume(float radious, float height) {
+        float cylinderVolume = 0.0f;
+        cylinderVolume = this.area(radious) * height;
+        return cylinderVolume;
+    }
+
+    public static void main(String[] args) {
+        float radious = 2, height = 3.5f;
+
+        Cylinder obj_01 = new Cylinder();
+        System.out.println("Volume of the cylinder is " + obj_01.volume(radious, height));
+    }
+}
